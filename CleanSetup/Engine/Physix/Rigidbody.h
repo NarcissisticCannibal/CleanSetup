39a9cbb1368c0bfbde0da1247df9a7ebd247f6b8
NarcissisticCannibal <profeldmar@gmail.com>
2018-07-10 23:14:35 +0200

    Started Rigidbody Physics

---
+++ CleanSetup/Engine/Physix/Rigidbody.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "../Math/Vector3.h"
+
+class Rigidbody {
+public:
+
+
+
+private:
+
+	Vector3* pos; //points at the position of the corresponding sprite
+	Vector3* scale;
+	float* rot;
+
+};
